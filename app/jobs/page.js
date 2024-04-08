ad1b805982b969442e47651765801e6aaeacbee6
Justin Duong <jduong@umich.edu>
2024-04-08 18:31:50 -0400

    Initialized repo. first commit.

---
+++ app/jobs/page.js
@@ -0,0 +1,9 @@
+import styles from '../page.module.css';
+
+export default function Jobs() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.jobs}>This is the jobs page.</div>
+    </main>
+  )
+}
---
+++ app/jobs/pld/page.js
@@ -0,0 +1,9 @@
+import styles from '../../page.module.css';
+
+export default function PaladinHome() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.jobs}>This is the Paladin Page.</div>
+    </main>
+  )
+}
