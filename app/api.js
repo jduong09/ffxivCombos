3b7fbe90408c0eb5563ebe0821af44295b0fd6fd
Justin Duong <jduong@umich.edu>
2024-04-21 19:26:58 -0400

    Finished json data for tanks, moving onto healers.

--- app/api.js
+++ app/api.js
@@ -773,10 +773,506 @@ const ffxivActions = {
         },
       },
       "dark knight": {
-
+        "Hard Slash": {
+          icon: {
+            set: '003000',
+            id: '003051'
+          }
+        },
+        "Syphon Strike": {
+          icon: {
+            set: '003000',
+            id: '003051'
+          }
+        },
+        "Unleash": {
+          icon: {
+            set: '003000',
+            id: '003063'
+          }
+        },
+        "Grit": {
+          icon: {
+            set: '003000',
+            id: '003070'
+          }
+        },
+        "Unmend": {
+          icon: {
+            set: '003000',
+            id: '003062'
+          }
+        },
+        "Souleater": {
+          icon: {
+            set: '003000',
+            id: '003055'
+          }
+        },
+        "Flood of Darkness": {
+          icon: {
+            set: '003000',
+            id: '003082'
+          }
+        },
+        "Blood Weapon": {
+          icon: {
+            set: '003000',
+            id: '003071'
+          }
+        },
+        "Shadow Wall": {
+          icon: {
+            set: '003000',
+            id: '003075'
+          }
+        },
+        "Stalwart Soul": {
+          icon: {
+            set: '003000',
+            id: '003084'
+          }
+        },
+        "Edge of Darkness": {
+          icon: {
+            set: '003000',
+            id: '003083'
+          }
+        },
+        "Dark Mind": {
+          icon: {
+            set: '003000',
+            id: '003076'
+          }
+        },
+        "Living Dead": {
+          icon: {
+            set: '003000',
+            id: '003077'
+          }
+        },
+        "Salted Earth": {
+          icon: {
+            set: '003000',
+            id: '003066'
+          }
+        },
+        "Plunge": {
+          icon: {
+            set: '003000',
+            id: '003061'
+          }
+        },
+        "Abyssal Drain": {
+          icon: {
+            set: '003000',
+            id: '003064'
+          }
+        },
+        "Carve and Spit": {
+          icon: {
+            set: '003000',
+            id: '003058'
+          }
+        },
+        "Bloodspiller": {
+          icon: {
+            set: '003000',
+            id: '003080'
+          }
+        },
+        "Quietus": {
+          icon: {
+            set: '003000',
+            id: '003079'
+          }
+        },
+        "Delirium": {
+          icon: {
+            set: '003000',
+            id: '003078'
+          }
+        },
+        "The Blackest Night": {
+          icon: {
+            set: '003000',
+            id: '003081'
+          }
+        },
+        "Flood of Shadow": {
+          icon: {
+            set: '003000',
+            id: '003082'
+          }
+        },
+        "Edge of Shadow": {
+          icon: {
+            set: '003000',
+            id: '003086'
+          }
+        },
+        "Dark Missionary": {
+          icon: {
+            set: '003000',
+            id: '003087'
+          }
+        },
+        "Living Shadow": {
+          icon: {
+            set: '003000',
+            id: '003088'
+          }
+        },
+        "Oblation": {
+          icon: {
+            set: '003000',
+            id: '003089'
+          }
+        },
+        "Salt and Darkness": {
+          icon: {
+            set: '003000',
+            id: '003090'
+          }
+        },
+        "Shadowbringer": {
+          icon: {
+            set: '003000',
+            id: '003091'
+          }
+        },
       },
       "gunbreaker": {
+        "Keen Edge": {
+          icon: {
+            set: '003000',
+            id: '003401'
+          }
+        },
+        "No Mercy": {
+          icon: {
+            set: '003000',
+            id: '003042'
+          }
+        },
+        "Brutal Shell": {
+          icon: {
+            set: '003000',
+            id: '003043'
+          }
+        },
+        "Camouflage": {
+          icon: {
+            set: '003000',
+            id: '003404'
+          }
+        },
+        "Demon Slice": {
+          icon: {
+            set: '003000',
+            id: '003405'
+          }
+        },
+        "Royal Guard": {
+          icon: {
+            set: '003000',
+            id: '003406'
+          }
+        },
+        "Lightning Shot": {
+          icon: {
+            set: '003000',
+            id: '003407'
+          }
+        },
+        "Danger Zone": {
+          icon: {
+            set: '003000',
+            id: '003408'
+          }
+        },
+        "Solid Barrel": {
+          icon: {
+            set: '003000',
+            id: '003409'
+          }
+        },
+        "Burst Strike": {
+          icon: {
+            set: '003000',
+            id: '003414'
+          }
+        },
+        "Nebula": {
+          icon: {
+            set: '003000',
+            id: '003412'
+          }
+        },
+        "Demon Slaughter": {
+          icon: {
+            set: '003000',
+            id: '003413'
+          }
+        },
+        "Aurora": {
+          icon: {
+            set: '003000',
+            id: '003415'
+          }
+        },
+        "Superbolide": {
+          icon: {
+            set: '003000',
+            id: '003416'
+          }
+        },
+        "Sonic Break": {
+          icon: {
+            set: '003000',
+            id: '003417'
+          }
+        },
+        "Rough Divide": {
+          icon: {
+            set: '003000',
+            id: '003418'
+          }
+        },
+        "Gnashing Fang": {
+          icon: {
+            set: '003000',
+            id: '003410'
+          }
+        },
+        "Savage Claw": {
+          icon: {
+            set: '003000',
+            id: '003414'
+          }
+        },
+        "Wicked Talon": {
+          icon: {
+            set: '003000',
+            id: '003411'
+          }
+        },
+        "Bow Shock": {
+          icon: {
+            set: '003000',
+            id: '003423'
+          }
+        },
+        "Heart of Light": {
+          icon: {
+            set: '003000',
+            id: '003424'
+          }
+        },
+        "Heart of Stone": {
+          icon: {
+            set: '003000',
+            id: '003425'
+          }
+        },
+        "Continuation": {
+          icon: {
+            set: '003000',
+            id: '003419'
+          }
+        },
+        "Jugular Rip": {
+          icon: {
+            set: '003000',
+            id: '003420'
+          }
+        },
+        "Abdomen Tear": {
+          icon: {
+            set: '003000',
+            id: '003421'
+          }
+        },
+        "Eye Gouge": {
+          icon: {
+            set: '003000',
+            id: '003422'
+          }
+        },
+        "Fated Circle": {
+          icon: {
+            set: '003000',
+            id: '003427'
+          }
+        },
+        "Bloodfest": {
+          icon: {
+            set: '003000',
+            id: '003428'
+          }
+        },
+        "Blasting Zone": {
+          icon: {
+            set: '003000',
+            id: '003429'
+          }
+        },
+        "Heart of Corundum": {
+          icon: {
+            set: '003000',
+            id: '003430'
+          }
+        },
+        "Hypervelocity": {
+          icon: {
+            set: '003000',
+            id: '003431'
+          }
+        },
+        "Double Down": {
+          icon: {
+            set: '003000',
+            id: '003432'
+          }
+        },
+      }
+    },
+    "healer": {
+      "scholar": {
+        "Ruin": {
+          icon: {
+            set: '000000',
+            id: '00501'
+          }
+        },
+        "Bio": {
+          icon: {
+            set: '000000',
+            id: '000503'
+          }
+        },
+        "Physick": {
+          icon: {
+            set: '000000',
+            id: '000518'
+          }
+        },
+        "Summon Eos": {
+          icon: {
+            set: '002000',
+            id: '002823'
+          }
+        },
+        "Resurrection": {
+          icon: {
+            set: '000000',
+            id: '000511'
+          }
+        },
+        "Whispering Dawn": {
+          icon: {
+            set: '002000',
+            id: '002852'
+          }
+        },
+        "Bio II": {
+          icon: {
+            set: '000000',
+            id: '000504'
+          }
+        },
+        "Adloquium": {
+          icon: {
+            set: '002000',
+            id: '002801'
+          }
+        },
+        "Succor": {
+          icon: {
+            set: '002000',
+            id: '002802'
+          }
+        },
+
+
+
         
+        "Ruin II": {
+          icon: {
+            set: '003000',
+            id: '003414'
+          }
+        },
+        "Fey Illumination": {
+          icon: {
+            set: '003000',
+            id: '003414'
+          }
+        },
+        "Aetherflow": {
+          icon: {
+            set: '003000',
+            id: '003414'
+          }
+        },
+        "Energy Drain": {
+          icon: {
+            set: '003000',
+            id: '003414'
+          }
+        },
+
+      },
+      "white mage": {
+
+      },
+      "astrologian": {
+
+      },
+      "sage": {
+
+      }
+    },
+    "meleeDps": {
+      "monk": {
+        
+      },
+      "dragoon": {
+
+      },
+      "ninja": {
+
+      },
+      "samurai": {
+
+      },
+      "reaper": {
+
+      }
+    },
+    "physicalRangedDps": {
+      "bard": {
+
+      },
+      "machinist": {
+
+      },
+      "dancer": {
+
+      }
+    },
+    "magicalRangedDps": {
+      "black mage": {
+
+      },
+      "red mage": {
+
+      },
+      "summoner": {
+
+      },
+      "blue mage": {
+
       }
     }
   }
