16ecdf9663dbb1c60bca33fb852460dff73bd289
Justin Duong <jduong@umich.edu>
2024-04-24 20:16:33 -0400

    Changed import path for prod.

--- app/jobs/pld/page.js
+++ app/jobs/pld/page.js
@@ -1,4 +1,4 @@
-import styles from '../../page.module.css';
+import styles from '@/app/page.module.css';
 import Image from 'next/image';
 import TankPageCombos from '@/components/jobCombos/tank/tankPageCombos';
 import DefensiveCooldowns from '@/components/jobCombos/tank/defensiveCooldown';
