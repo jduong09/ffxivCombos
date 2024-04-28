7dff2f71fe7d7cf0d7be4d0a47ce547d02fd9348
Justin Duong <jduong@umich.edu>
2024-04-28 18:58:11 -0400

    Added css module for landingPage.

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import styles from "./page.module.css";
 import Image from "next/image";
+import landingPage from "@/app/landingPage.module.css";
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
         <nav>
           <ul className={styles.navList}>
             <li className={styles.navListItem}>
-              <a className={styles.navListLink} href="/jobs">Jobs</a>
+              <a className={styles.navListLink} href="/classes">Classes</a>
             </li>
             <li className={styles.navListItem}>
               <a className={styles.navListLink} href="/jobs">Fashion</a>
@@ -23,7 +24,29 @@ export default function Home() {
         </nav>
       </header>
       <main className={styles.main}>
-        <div>Edit Landing Page.</div>
+        <div>
+          <div>
+            <h1>From Sprout to Blossomed Player</h1>
+            <h2>Soppos Guide is filled with dps combos, dungeons pack rotation advice, role action use and job glam.</h2>
+          </div>
+          <div>
+            <Image id='img-struggling' src={'/emotes/soppo_sad_crop.png'} width='344' height='489' />
+          </div>
+        </div>
+        <div className={landingPage.divStruggles}>
+          <ul className={landingPage.listStruggles}>
+            <li>
+              <h3>Forgot Rotations?</h3>
+            </li>
+            <li>
+              <h3>Bottom of Damage List?</h3>
+            </li>
+            <li>
+              <h3>Always Floor Tank?</h3>
+            </li>
+          </ul>
+          <a href='/classes'>Check out classes</a>
+        </div>
       </main>
     </div>
   );
