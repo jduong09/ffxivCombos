506bb78ee8b86b88b70b27812634ae3b168980a1
Justin Duong <jduong@umich.edu>
2024-05-08 19:28:29 -0400

    Added images for each job, added lists for navigating to each specific class.

--- app/jobs/page.js
+++ app/jobs/page.js
@@ -1,5 +1,4 @@
 import styles from '../page.module.css';
-import landingPage from '@/app/landingPage.module.css';
 import jobStyles from '@/app/jobs/job.module.css';
 import HeaderComponent from '@/components/htmlComponent/header';
 import Image from 'next/image';
@@ -9,70 +8,6 @@ export default function Jobs() {
     <div>
       <HeaderComponent />
       <main className={styles.main}>
-        {/*
-        <ul className={jobStyles.listJobs}>
-          <li>
-            <div>
-              <h2>Tanks</h2>
-              <div className={jobStyles.curtain}>
-                <div className={jobStyles.curtainWrapper}>
-                  <input ref={tankRef} className={jobStyles.curtainInput} type='checkbox' checked={tankIsChecked} onChange={() => setTankIsChecked(!tankIsChecked)} />
-                  <div className={`${jobStyles.curtainPanel} ${jobStyles.curtainPanelLeft}`}>
-                    <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/paladin.png' alt='class icon for paladin' />
-                    <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/warrior.png' alt='class icon for warrior' />
-                  </div>
-                  <div className={jobStyles.curtainContent}>
-                    <ul>
-                      <li><a href="/jobs/pld">Paladin</a></li>
-                      <li><a href="/jobs/war">Warrior</a></li>
-                      <li><a href="/jobs/gnb">Gunbreaker</a></li>
-                      <li><a href="/jobs/drk">Dark Knight</a></li>
-                    </ul>
-                  </div>
-                  <div className={`${jobStyles.curtainPanel} ${jobStyles.curtainPanelRight}`}>
-                    <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/gunbreaker.png' alt='class icon for gunbreaker' />
-                    <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/darkknight.png' alt='class icon for dark knight' />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <h2>Healers</h2>
-              <ul>
-                <li><a href="/jobs/sch">Scholar</a></li>
-                <li><a href="/jobs/whm">White Mage</a></li>
-                <li><a href="/jobs/ast">Astrologian</a></li>
-                <li><a href="/jobs/sge">Sage</a></li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <div>
-              <h2>Ranged DPS</h2>
-              <ul>
-                <li><a href="/jobs/blm">Black Mage</a></li>
-                <li><a href="/jobs/smn">Summoner</a></li>
-                <li><a href="/jobs/mch">Machinist</a></li>
-                <li><a href="/jobs/dnc">Dancer</a></li>
-                <li><a href="/jobs/rdm">Red Mage</a></li>
-              </ul>   
-            </div>
-          </li>
-          <li>
-            <div>
-              <h2>Melee DPS</h2>
-              <ul>
-                <li><a href="/jobs/sam">Samurai</a></li>
-                <li><a href="/jobs/nin">Ninja</a></li>
-                <li><a href="/jobs/mnk">Monk</a></li>
-                <li><a href="/jobs/drg">Dragoon</a></li>
-                <li><a href="/jobs/rpr">Reaper</a></li>
-              </ul>
-            </div>
-          </li>
-        </ul>*/}
         <div className={jobStyles.divMain}>
           <ul className={jobStyles.listClassesContainer}>
             <li className={jobStyles.listItemClassContainer}>
@@ -85,7 +20,7 @@ export default function Jobs() {
                     </div>
                     <div className={jobStyles.content}>
                       <span>Check out Guide</span>
-                      <Image className={jobStyles.contentImageSoppo} src={'/fashion/soppo_gunbreaker_4_29_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
+                      <Image className={jobStyles.contentImageSoppo} src={'/poses_generic/gnb_generic_05_08_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
                     </div>
                   </a>
                 </li>
@@ -97,7 +32,7 @@ export default function Jobs() {
                     </div>
                     <div className={jobStyles.content}>
                       <span>Check out Guide</span>
-                      <Image className={jobStyles.contentImageSoppo} src={'/fashion/soppo_gunbreaker_4_29_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
+                      <Image className={jobStyles.contentImageSoppo} src={'/poses_generic/pld_generic_05_08_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
                     </div>
                   </a>
                 </li>
@@ -109,7 +44,7 @@ export default function Jobs() {
                     </div>
                     <div className={jobStyles.content}>
                       <span>Check out Guide</span>
-                      <Image className={jobStyles.contentImageSoppo} src={'/fashion/soppo_gunbreaker_4_29_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
+                      <Image className={jobStyles.contentImageSoppo} src={'/poses_generic/war_generic_05_08_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
                     </div>
                   </a>
                 </li>
@@ -121,7 +56,7 @@ export default function Jobs() {
                     </div>
                     <div className={jobStyles.content}>
                       <span>Check out Guide</span>
-                      <Image className={jobStyles.contentImageSoppo} src={'/fashion/soppo_gunbreaker_4_29_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
+                      <Image className={jobStyles.contentImageSoppo} src={'/poses_generic/drk_generic_05_08_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
                     </div>
                   </a>
                 </li>
@@ -135,11 +70,11 @@ export default function Jobs() {
                   <a href="/jobs/sch">
                     <div className={jobStyles.cover}>
                       <h3>Scholar</h3>
-                      <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/gunbreaker.png' alt='class icon for gunbreaker' />
+                      <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/scholar.png' alt='class icon for gunbreaker' />
                     </div>
                     <div className={jobStyles.content}>
                       <span>Check out Guide</span>
-                      <Image className={jobStyles.contentImageSoppo} src={'/fashion/soppo_gunbreaker_4_29_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
+                      <Image className={jobStyles.contentImageSoppo} src={'/poses_generic/sch_generic_05_08_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
                     </div>
                   </a>
                 </li>
@@ -147,11 +82,11 @@ export default function Jobs() {
                   <a href="/jobs/whm">
                     <div className={jobStyles.cover}>
                       <h3>White Mage</h3>
-                      <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/paladin.png' alt='class icon for paladin' />
+                      <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/whitemage.png' alt='class icon for paladin' />
                     </div>
                     <div className={jobStyles.content}>
                       <span>Check out Guide</span>
-                      <Image className={jobStyles.contentImageSoppo} src={'/fashion/soppo_gunbreaker_4_29_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
+                      <Image className={jobStyles.contentImageSoppo} src={'/poses_generic/whm_generic_05_08_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
                     </div>
                   </a>
                 </li>
@@ -159,11 +94,11 @@ export default function Jobs() {
                   <a href="/jobs/ast">
                     <div className={jobStyles.cover}>
                       <h3>Astrologian</h3>
-                      <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/warrior.png' alt='class icon for warrior' />
+                      <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/astrologian.png' alt='class icon for warrior' />
                     </div>
                     <div className={jobStyles.content}>
                       <span>Check out Guide</span>
-                      <Image className={jobStyles.contentImageSoppo} src={'/fashion/soppo_gunbreaker_4_29_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
+                      <Image className={jobStyles.contentImageSoppo} src={'/poses_generic/ast_generic_05_08_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
                     </div>
                   </a>
                 </li>
@@ -171,11 +106,153 @@ export default function Jobs() {
                   <a href="/jobs/sge">
                     <div className={jobStyles.cover}>
                       <h3>Sage</h3>
-                      <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/darkknight.png' alt='class icon for dark knight' />
+                      <Image className={jobStyles.imageClassIcon} src={'/classIcons/sage.png'} width='0' height='0' sizes='100vw' alt='class icon sage' />
                     </div>
                     <div className={jobStyles.content}>
                       <span>Check out Guide</span>
-                      <Image className={jobStyles.contentImageSoppo} src={'/fashion/soppo_gunbreaker_4_29_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
+                      <Image className={jobStyles.contentImageSoppo} src={'/poses_generic/sch_generic_05_08_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </li>
+
+            <li className={jobStyles.listItemClassContainer}>
+              <ul className={jobStyles.listSixSquares}>
+                <li className={jobStyles.book}>
+                  <a href="/jobs/sch">
+                    <div className={jobStyles.cover}>
+                      <h3>Monk</h3>
+                      <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/monk.png' alt='class icon for gunbreaker' />
+                    </div>
+                    <div className={jobStyles.content}>
+                      <span>Check out Guide</span>
+                      <Image className={jobStyles.contentImageSoppo} src={'/poses_generic/mnk_generic_05_08_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
+                    </div>
+                  </a>
+                </li>
+                <li className={jobStyles.book}>
+                  <a href="/jobs/whm">
+                    <div className={jobStyles.cover}>
+                      <h3>Ninja</h3>
+                      <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/ninja.png' alt='class icon for paladin' />
+                    </div>
+                    <div className={jobStyles.content}>
+                      <span>Check out Guide</span>
+                      <Image className={jobStyles.contentImageSoppo} src={'/poses_generic/nin_generic_05_08_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
+                    </div>
+                  </a>
+                </li>
+                <li className={jobStyles.book}>
+                  <a href="/jobs/ast">
+                    <div className={jobStyles.cover}>
+                      <h3>Samurai</h3>
+                      <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/samurai.png' alt='class icon for warrior' />
+                    </div>
+                    <div className={jobStyles.content}>
+                      <span>Check out Guide</span>
+                      <Image className={jobStyles.contentImageSoppo} src={'/poses_generic/sam_generic_05_08_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
+                    </div>
+                  </a>
+                </li>
+                <li className={jobStyles.book}>
+                  <a href="/jobs/sge">
+                    <div className={jobStyles.cover}>
+                      <h3>Dragoon</h3>
+                      <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/dragoon.png' alt='class icon for dark knight' />
+                    </div>
+                    <div className={jobStyles.content}>
+                      <span>Check out Guide</span>
+                      <Image className={jobStyles.contentImageSoppo} src={'/poses_generic/drg_generic_05_08_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
+                    </div>
+                  </a>
+                </li>
+                <li className={jobStyles.book}>
+                  <a href="/jobs/sge">
+                    <div className={jobStyles.cover}>
+                      <h3>Reaper</h3>
+                      <Image className={jobStyles.imageClassIcon} src={'/classIcons/reaper.png'} width='0' height='0' sizes='100vw' alt='class icon sage' />
+                    </div>
+                    <div className={jobStyles.content}>
+                      <span>Check out Guide</span>
+                      <Image className={jobStyles.contentImageSoppo} src={'/poses_generic/rpr_generic_05_08_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </li>
+
+            <li className={jobStyles.listItemClassContainer}>
+              <ul className={jobStyles.listSixSquares}>
+                <li className={jobStyles.book}>
+                  <a href="/jobs/sch">
+                    <div className={jobStyles.cover}>
+                      <h3>Black Mage</h3>
+                      <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/blackmage.png' alt='class icon for gunbreaker' />
+                    </div>
+                    <div className={jobStyles.content}>
+                      <span>Check out Guide</span>
+                      <Image className={jobStyles.contentImageSoppo} src={'/poses_generic/blm_generic_05_08_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
+                    </div>
+                  </a>
+                </li>
+                <li className={jobStyles.book}>
+                  <a href="/jobs/whm">
+                    <div className={jobStyles.cover}>
+                      <h3>Red Mage</h3>
+                      <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/redmage.png' alt='class icon for paladin' />
+                    </div>
+                    <div className={jobStyles.content}>
+                      <span>Check out Guide</span>
+                      <Image className={jobStyles.contentImageSoppo} src={'/poses_generic/rdm_generic_05_08_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
+                    </div>
+                  </a>
+                </li>
+                <li className={jobStyles.book}>
+                  <a href="/jobs/ast">
+                    <div className={jobStyles.cover}>
+                      <h3>Summoner</h3>
+                      <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/summoner.png' alt='class icon for warrior' />
+                    </div>
+                    <div className={jobStyles.content}>
+                      <span>Check out Guide</span>
+                      <Image className={jobStyles.contentImageSoppo} src={'/poses_generic/smn_generic_05_08_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
+                    </div>
+                  </a>
+                </li>
+                <li className={jobStyles.book}>
+                  <a href="/jobs/sch">
+                    <div className={jobStyles.cover}>
+                      <h3>Machinist</h3>
+                      <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/machinist.png' alt='class icon for gunbreaker' />
+                    </div>
+                    <div className={jobStyles.content}>
+                      <span>Check out Guide</span>
+                      <Image className={jobStyles.contentImageSoppo} src={'/poses_generic/mch_generic_05_08_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
+                    </div>
+                  </a>
+                </li>
+                <li className={jobStyles.book}>
+                  <a href="/jobs/whm">
+                    <div className={jobStyles.cover}>
+                      <h3>Dancer</h3>
+                      <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/dancer.png' alt='class icon for paladin' />
+                    </div>
+                    <div className={jobStyles.content}>
+                      <span>Check out Guide</span>
+                      <Image className={jobStyles.contentImageSoppo} src={'/poses_generic/dnc_generic_05_08_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
+                    </div>
+                  </a>
+                </li>
+                <li className={jobStyles.book}>
+                  <a href="/jobs/whm">
+                    <div className={jobStyles.cover}>
+                      <h3>Bard</h3>
+                      <img className={jobStyles.imageClassIcon} src='https://xivapi.com/cj/1/bard.png' alt='class icon for paladin' />
+                    </div>
+                    <div className={jobStyles.content}>
+                      <span>Check out Guide</span>
+                      <Image className={jobStyles.contentImageSoppo} src={'/poses_generic/brd_generic_05_08_24.png'} width='0' height='0' sizes="100vw" alt='image of gunbreaker soppo' />
                     </div>
                   </a>
                 </li>
