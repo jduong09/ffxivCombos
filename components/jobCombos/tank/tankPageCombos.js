3ea26477e951aa8b149e3f7cd62ae5071ace2bc0
Justin Duong <jduong@umich.edu>
2024-06-12 18:50:37 -0400

    Moved all css for classPage to separate css module named classPage.module.css

--- components/jobCombos/tank/tankPageCombos.js
+++ components/jobCombos/tank/tankPageCombos.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import jobStyles from '@/app/jobs/job.module.css';
+import classPageSyles from '@/app/jobs/classPage.module.css'
 import { ffxivActions } from '@/app/api';
 
 const TankPageCombos = ({ jobName, normalArray, burstArray, dungeonArray }) => {
@@ -31,58 +32,58 @@ const TankPageCombos = ({ jobName, normalArray, burstArray, dungeonArray }) => {
   const renderBurstDiv = () => {
     if (jobName === "gunbreaker") {
       return (
-        <div className={jobStyles.divComboWithVideo}>
-          <div className={jobStyles.divInfo}>
+        <div className={classPageSyles.divComboWithVideo}>
+          <div className={classPageSyles.divInfo}>
             <h2>Gunbreaker: 2 Minute Window</h2>
             <div>The key to Gunbreaker's burst combo is having 3 cartridges in the powder gauge. You obtain 1 cartridge for each 1-2-3 combo, and 3 cartridges for using the skill "Bloodfest". The goal is to either have <b>BloodFest</b> ready when <b>No Mercy</b> is coming off cooldown, or to have 3 cartridges in the powder gauge already prepared.</div>
             <div>Once you are prepared to enter the burst, you use <b>No Mercy</b> followed by either <b>Double Down</b> or <b>Gnashing Fang</b> combo. In this burst, we need to use our oGCDs <b>Sonic Break</b> <b>Blasting Zone</b> <b>Bow Shock</b>.</div>
           </div>
-          <div className={jobStyles.videoContainer}>
+          <div className={classPageSyles.videoContainer}>
             <iframe id="video-pld-burst" src="https://www.youtube.com/embed/i-hsMqDW2-c" title='paladin burst combo' allowFullScreen/>
           </div>
-          <ul className={jobStyles.listJobAction}>{listItemsBurst}</ul>
+          <ul className={classPageSyles.listJobAction}>{listItemsBurst}</ul>
         </div>
       );
     } else if (jobName === "paladin") {
       return (
-        <div className={jobStyles.divComboWithVideo}>
-          <div className={jobStyles.divInfo}>
+        <div className={classPageSyles.divComboWithVideo}>
+          <div className={classPageSyles.divInfo}>
             <h2>2 Minute Window</h2>
             <div>For Paladin, we want to optimize our burst by timing our 1-2-3 combo to finish right before <b>Fight or Flight</b> comes off cooldown. This will give us 1 free use of <b>Holy Spirit</b> to start our burst window. This will also give us 3 atonements to use at the end of the burst window.</div>
             <div>If done correctly, we should fit our whole combo in and finish with 2 of the 3 atonements used in the burst window.</div>
           </div>
-          <div className={jobStyles.videoContainer}>
+          <div className={classPageSyles.videoContainer}>
             <iframe id="video-pld-burst" src="https://www.youtube.com/embed/i-hsMqDW2-c" title='paladin burst combo' allowFullScreen/>
           </div>
-          <ul className={jobStyles.listJobAction}>{listItemsBurst}</ul>
+          <ul className={classPageSyles.listJobAction}>{listItemsBurst}</ul>
         </div>
       )
     } else if (jobName === "dark knight") {
       return (
-        <div className={jobStyles.divComboWithVideo}>
-          <div className={jobStyles.divInfo}>
+        <div className={classPageSyles.divComboWithVideo}>
+          <div className={classPageSyles.divInfo}>
             <h2>2 Minute Window</h2>
             <div>Dark Knight's burst window requires you to have the buff Darkside up. This means that we need MP, because we will use <b>Edge of Darkness</b> (3000 MP).</div>
             <div>Once we have Darkside up, we can start our burst window. Burn through our MP with Edge of Darkness Spam, weaving in our oGCDs (Carve and Spit & Bloodspiller). Then we can use Delirium to get 3 non-mp costing bloodspillers.</div>
           </div>
-          <div className={jobStyles.videoContainer}>
+          <div className={classPageSyles.videoContainer}>
             <iframe id="video-pld-burst" src="https://www.youtube.com/embed/i-hsMqDW2-c" title='paladin burst combo' allowFullScreen/>
           </div>
-          <ul className={jobStyles.listJobAction}>{listItemsBurst}</ul>
+          <ul className={classPageSyles.listJobAction}>{listItemsBurst}</ul>
         </div>
       )
     } else {
       return (
-        <div className={jobStyles.divComboWithVideo}>
-          <div className={jobStyles.divInfo}>
+        <div className={classPageSyles.divComboWithVideo}>
+          <div className={classPageSyles.divInfo}>
             <h2>2 Minute Window</h2>
             <div>Warriors burst window is fairly simple. It's called Fell Cleave spam. We will start by buffing our damage with <b>Inner Release</b>. Follow that up with our biggest potency attack, <b>Primal Rend</b>.</div>
             <div>Using Inner Release, we execute 3 <b>Fell Cleaves</b>, follow that up with a upheaval, and then use Infuriate to increase our beast gauge and execute the upgraded Fell Cleave, <b>Inner Chaos</b>.</div>
           </div>
-          <div className={jobStyles.videoContainer}>
+          <div className={classPageSyles.videoContainer}>
             <iframe id="video-pld-burst" src="https://www.youtube.com/embed/i-hsMqDW2-c" title='paladin burst combo' allowFullScreen/>
           </div>
-          <ul className={jobStyles.listJobAction}>{listItemsBurst}</ul>
+          <ul className={classPageSyles.listJobAction}>{listItemsBurst}</ul>
         </div>
       )
     }
@@ -91,9 +92,9 @@ const TankPageCombos = ({ jobName, normalArray, burstArray, dungeonArray }) => {
   const renderNormalDiv = () => {
     if (jobName === "gunbreaker") {
       return (
-        <div className={jobStyles.divComboNoVideo}>
-          <ul className={jobStyles.listJobAction}>{listItemsNormal}</ul>
-          <div className={jobStyles.divInfo}>
+        <div className={classPageSyles.divComboNoVideo}>
+          <ul className={classPageSyles.listJobAction}>{listItemsNormal}</ul>
+          <div className={classPageSyles.divInfo}>
             <h2>Normal Combo</h2>
             <div>Gunbreaker's normal combo of <b>Keen Edge</b> - <b>Brutal Shell</b> - <b>Solid Barrel</b> is simple enough. Using this combo gives the Gunbreaker a barrier, and also restores their own hp. The other benefit of the 1-2-3 combo is at the combo action of Solid barrel will add a cartridge to your powder gauge for when you need it in burst phase.</div>
             <div>A couple of things to remember when playing gunbreaker. We should always be using our 1-2-3 combo while not in burst phase. If we are ever going to be capped on cartridges, remember to use <b>Burst Strike</b> followed by <b>Hypervelocity</b>. Finally, one of our defensive mitigation skills, <b>Heart of Stone</b> (<b>Heart of Corundum</b> at higher levels). When using either ability while under the buff <b>Brutal Shell</b> from the 1-2-3 combo, we can give the effects to another party member.</div>
@@ -102,9 +103,9 @@ const TankPageCombos = ({ jobName, normalArray, burstArray, dungeonArray }) => {
       )
     } else if (jobName === "paladin") {
       return (
-        <div className={jobStyles.divComboNoVideo}>
-          <ul className={jobStyles.listJobAction}>{listItemsNormal}</ul>
-          <div className={jobStyles.divInfo}>
+        <div className={classPageSyles.divComboNoVideo}>
+          <ul className={classPageSyles.listJobAction}>{listItemsNormal}</ul>
+          <div className={jobclassPageSylesStyles.divInfo}>
             <h2>Normal Combo</h2>
             <div>Paladin's normal combo at early levels does not grant any buffs or benefits. When <b>Royal Authority</b> is unlocked at level 60, finishing the <b>Fast Blade</b> - <b>Riot Blade</b> - <b>Royal Authority</b> combo grants Paladin 3 stacks of <b>Sword Oath</b> and <b>Divine Might</b>.</div>
             <div>The 3 stacks of Sword's Oath grant the Paladin the ability to use <b>Atonement</b>, a GCD attack that will restore MP. The other buff we get is Divine Might, will will allow us to use one <b>Holy Spirit</b> or <b>Holy Circle</b> without the cast time, making those spells instant for one attack.</div>
@@ -114,9 +115,9 @@ const TankPageCombos = ({ jobName, normalArray, burstArray, dungeonArray }) => {
       )
     } else if (jobName === "dark knight") {
       return (
-        <div className={jobStyles.divComboNoVideo}>
-          <ul className={jobStyles.listJobAction}>{listItemsNormal}</ul>
-          <div className={jobStyles.divInfo}>
+        <div className={classPageSyles.divComboNoVideo}>
+          <ul className={classPageSyles.listJobAction}>{listItemsNormal}</ul>
+          <div className={classPageSyles.divInfo}>
             <h2>Normal Combo</h2>
             <div>Dark Knight's 1-2-3 combo of <b>Hard Slash</b> - <b>Syphon Strike</b> - <b>Souleater</b> restore both HP and MP, while increasing the Blood Gauge by 20 for one successful combo.</div>
             <div>Due to how MP expensive Dark Knight's damage output is, it is important to do the combo correctly. When Dark Knight's Blood Gauge has reached 50, we can cast <b>Bloodspiller</b>.</div>
@@ -125,9 +126,9 @@ const TankPageCombos = ({ jobName, normalArray, burstArray, dungeonArray }) => {
       )
     } else {
       return (
-        <div className={jobStyles.divComboNoVideo}>
-          <ul className={jobStyles.listJobAction}>{listItemsNormal}</ul>
-          <div className={jobStyles.divInfo}>
+        <div className={classPageSyles.divComboNoVideo}>
+          <ul className={classPageSyles.listJobAction}>{listItemsNormal}</ul>
+          <div className={classPageSyles.divInfo}>
             <h2>Normal Combo</h2>
             <div><b>Heavy Swing</b> - <b>Maim</b> - <b>Storm's Path</b> grants the Warrior 30 Beast Gauge, and restores their own HP by a potency of 250.</div>
             <div>The Beast Gauge will be used for the burst combo, where we use a bunch of Fell Cleave's. Outside of the normal 1-2-3 combo, there is a 1-2-3 combo that will grant the user increased damage, which is <b>Heavy Swing</b> - <b>Maim</b> - <b>Storm's Eye</b>.</div>
@@ -141,9 +142,9 @@ const TankPageCombos = ({ jobName, normalArray, burstArray, dungeonArray }) => {
   const renderDungeonDiv = () => {
     if (jobName === "gunbreaker") {
       return (
-        <div className={jobStyles.divComboNoVideo}>
-          <ul className={jobStyles.listJobAction}>{listItemsDungeon}</ul>
-          <div className={jobStyles.divInfo}>
+        <div className={classPageSyles.divComboNoVideo}>
+          <ul className={classPageSyles.listJobAction}>{listItemsDungeon}</ul>
+          <div className={classPageSyles.divInfo}>
             <h2>Dungeon Combos</h2>
             <div>During dungeons, Gunbreaker has access to <b>Demon Slice</b> and <b>Demon Slaughter</b>. Fulfilling this combo will give the Gunbreaker a cartridge to your powder gauge.</div>
             <div>With our cartridge, we can use <b>Fated Circle</b> for 1 cartridge, and <b>Double Down</b> for 2 cartridges.</div>
@@ -153,9 +154,9 @@ const TankPageCombos = ({ jobName, normalArray, burstArray, dungeonArray }) => {
       )
     } else if (jobName === "paladin") {
       return (
-        <div className={jobStyles.divComboNoVideo}>
-          <ul className={jobStyles.listJobAction}>{listItemsDungeon}</ul>
-          <div className={jobStyles.divInfo}>
+        <div className={classPageSyles.divComboNoVideo}>
+          <ul className={classPageSyles.listJobAction}>{listItemsDungeon}</ul>
+          <div className={classPageSyles.divInfo}>
             <h2>Dungeon Combos</h2>
             <div>In dungeons we will primarily using our AOEs, <b>Total Eclipse</b> and <b>Prominence</b>. Use <b>Spirits Within</b>/<b>Expiacion</b> and <b>Circle of Scorn</b> when these abilities are off cooldown.</div>
             <div>Finish the combo of <b>Prominence</b> grants Paladin one stack of Divine Might, allowing for a third AOE attack, <b>Holy Circle</b> to be used with no mana cost.</div>
@@ -165,9 +166,9 @@ const TankPageCombos = ({ jobName, normalArray, burstArray, dungeonArray }) => {
       )
     } else if (jobName === "dark knight") {
       return (
-        <div className={jobStyles.divComboNoVideo}>
-          <ul className={jobStyles.listJobAction}>{listItemsDungeon}</ul>
-          <div className={jobStyles.divInfo}>
+        <div className={classPageSyles.divComboNoVideo}>
+          <ul className={classPageSyles.listJobAction}>{listItemsDungeon}</ul>
+          <div className={classPageSyles.divInfo}>
             <h2>Dungeon Combos</h2>
             <div>Dark Knight has access to <b>Unleash</b>, followed by <b>Stalwart Soul</b>. Due to Dark Knight's use of mana for their oGCD's, Stalwart Soul will restore mp.</div>
             <div><b>Flood of Darkness</b> is Dark's Knight ability to spam when you have the mana for it. It will grant Darkside, increase damage dealt by 10%, extended up to a duration of 60 seconds.</div>
@@ -177,9 +178,9 @@ const TankPageCombos = ({ jobName, normalArray, burstArray, dungeonArray }) => {
       )
     } else {
       return (
-        <div className={jobStyles.divComboNoVideo}>
-          <ul className={jobStyles.listJobAction}>{listItemsDungeon}</ul>
-          <div className={jobStyles.divInfo}>
+        <div className={classPageSyles.divComboNoVideo}>
+          <ul className={classPageSyles.listJobAction}>{listItemsDungeon}</ul>
+          <div className={classPageSyles.divInfo}>
             <h2>Dungeon Combos</h2>
             <div>Warrior has access to abilities that will heal the Warrior in dungeon wall to walls if used in combination with the skill <b>Bloodwhetting</b>. Because much of the mitigation for Warrior comes from <b>Bloodwhetting</b> and healing, we can utilize this ability when our health is low, and maximize healing to full.</div>
             <div>Warrior uses <b>Overpower</b> and <b>Mythril Tempest</b>, granting Surging Tempest, increasing damage dealt by 10%, up to 60 seconds. This will also increase beast gauge by 20.</div>
@@ -191,7 +192,7 @@ const TankPageCombos = ({ jobName, normalArray, burstArray, dungeonArray }) => {
   }
 
   return (
-    <div className={jobStyles.divMainContainer}>
+    <div className={classPageSyles.divMainContainer}>
       {renderBurstDiv()}
       {renderNormalDiv()}
       {renderDungeonDiv()}
